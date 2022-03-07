f8514c4ce82bb9fd9a1808ec2b1f9b91dec02447
bernandaputri <haihaiputri@gmail.com>
2022-03-07 12:38:38 +0800

    commit

--- server.test.js
+++ server.test.js
@@ -6,13 +6,13 @@ const request = supertest(app)
 it('Call the /group endpoint', async done => {
     const res = await request.get('/group')
     expect(res.status).toBe(200)
-    expect(res.text).toBe('This is HAI MP 32')
+    expect(res.text).toBe('Ready to win! - HAI MP32')
     done()
 })
 it('Call the / endpoint', async done => {
     const res = await request.get('/')
     expect(res.status).toBe(200)
-    expect(res.text).toBe('This App is running properly!')
+    expect(res.text).toBe('This web app is created by HAI MP32')
     done()
 })
 it('Call the /greet endpoint', async done => {
