993d7546842e2dcb0e416c997b1e6317de67cfee
bernandaputri <haihaiputri@gmail.com>
2022-03-09 16:41:57 +0800

    update endpoint

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 app.get('/', (req,res) => res.send('This web app is created by HAI MP32'))
 app.get('/greet', (req,res) => res.send('Hi!'))
 app.get('/group', (req,res) => res.send('Ready to win! - HAI MP32'))
-app.get('/update', (req,res) => res.send('This page is already updated'))
+app.get('/update', (req,res) => res.send('This page is already updated!'))
 
 module.exports = app
