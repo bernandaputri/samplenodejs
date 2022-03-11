325f089be89512ffb133cd8d488fac3845e0921c
bernandaputri <haihaiputri@gmail.com>
2022-03-11 09:05:20 +0800

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => res.send('This web app is created by HAI MP32'))
-app.get('/greet', (req,res) => res.send('Hi!'))
+app.get('/greet', (req,res) => res.send('Heyoo!'))
 app.get('/group', (req,res) => res.send('Ready to win! - HAI MP32'))
 
 module.exports = app
